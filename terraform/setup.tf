57e6b34dd8e0b18a79b98afe0910fddffa79dbac
Pedro Montez Alves <alifmontez@gmail.com>
2022-07-22 11:19:34 +0100

    fix2

--- terraform/setup.tf
+++ terraform/setup.tf
@@ -88,6 +88,14 @@ resource "aws_security_group" "sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "allow traffic from TCP/5000"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
